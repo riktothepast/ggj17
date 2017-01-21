da924fd97434540773190243ab4f0baf60b1a33b
unknown <moraleszez@gmail.com>
2017-01-21 13:57:12 -0600

    fixed issues with missing object

---
+++ Assets/Characters/Scripts/PowerStriker.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Characters
+{
+    public class PowerStriker : MonoBehaviour
+    {
+        public Transform leftArm;
+        public Transform leftHand;
+
+        private SpriteRenderer leftHandSprite;
+
+        private Quaternion orginArmRot;
+        private Vector3 handPosition;
+        private void Start()
+        {
+            leftHandSprite = leftHand.GetComponentInChildren<SpriteRenderer>();
+            orginArmRot = leftHandSprite.transform.localRotation;
+            handPosition = leftHandSprite.transform.localPosition;
+        }
+
+        public void HitPosition(Vector3 hitPosition)
+        {
+            leftHandSprite.transform.position = hitPosition;
+            leftHandSprite.transform.parent = null;
+        }
+
+        public void ResetPosition()
+        {
+            leftHandSprite.transform.SetParent(leftHand);
+            leftHandSprite.transform.localRotation = Quaternion.identity;
+            leftHandSprite.transform.localPosition = Vector3.zero;
+        }
+    }
+}
---
+++ Assets/Characters/Scripts/SpriteManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Characters
+{
+    public class SpriteManager : MonoBehaviour
+    {
+
+        private List<SpriteRenderer> bodyParts;
+        private Animator animator;
+
+        // Use this for initialization
+        private void Start()
+        {
+            animator = GetComponent<Animator>();
+            bodyParts = new List<SpriteRenderer>(GetComponentsInChildren<SpriteRenderer>());
+        }
+
+        public void ChangeColor(Color color)
+        {
+            for (int i = 0; i < bodyParts.Count; i++)
+            {
+                bodyParts[i].color = color;
+            }
+        }
+
+        public void SetRunSpeed(float normalizedRunDirection, float runSpeed)
+        {
+
+            animator.SetFloat("NormalRun", Mathf.Abs(normalizedRunDirection));
+            if ((int)normalizedRunDirection != 0)
+            {
+                transform.localScale = new Vector3(normalizedRunDirection, 1, 1);
+            }
+        }
+
+        public void SetRunSpeed(float normalizedRunDirection, float runSpeed, bool conditional)
+        {
+            if (conditional)
+            {
+                animator.SetFloat("NormalRun", Mathf.Abs(normalizedRunDirection));
+                if ((int)normalizedRunDirection != 0)
+                {
+                    transform.localScale = new Vector3(normalizedRunDirection, 1, 1);
+                }
+            }
+        }
+
+        public void PlayJump()
+        {
+            animator.SetBool("Jumping", true);
+        }
+
+        public void Land()
+        {
+            animator.SetBool("Jumping", false);
+        }
+    }
+}
