0f4ec00bf0698416cbc39cb1032937e17b55235e
riktothepast <rik@502studios.net>
2017-01-22 11:26:14 -0600

    override player grounded check

--- Assets/Scripts/CharacterController/CharacterController2D.cs
+++ Assets/Scripts/CharacterController/CharacterController2D.cs
@@ -145,7 +145,7 @@ public class CharacterController2D : MonoBehaviour
 	public CharacterCollisionState2D collisionState = new CharacterCollisionState2D();
 	[HideInInspector][NonSerialized]
 	public Vector3 velocity;
-	public bool isGrounded { get { return collisionState.below; } }
+	public bool isGrounded { get { return collisionState.below; } set { this.collisionState.below = value; } }
 
 	private const float kSkinWidthFloatFudgeFactor = 0.001f;
 
