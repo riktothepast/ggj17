5753204d881179fe513f510e477724d9abaab8aa
Vidsneezes <moraleszez@gmail.com>
2017-01-22 14:35:24 -0600

    adds skip to intro

---
+++ Assets/IntroSceneAssets/Scripts/SkipIntro.cs
@@ -0,0 +1,21 @@
+﻿   using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkipIntro : MonoBehaviour {
+
+    public MenuController menuController;
+
+	// Use this for initialization
+	void Start () {
+        menuController = GetComponent<MenuController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))
+        {
+            menuController.LoadScene("menuScene");
+        }
+	}
+}
