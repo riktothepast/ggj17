c4cdafd544b931aea812cf01041d1f2992a1bfc7
riktothepast <rik@502studios.net>
2017-01-22 11:42:29 -0600

    Adds lava pooler

---
+++ Assets/Scripts/Utils/Audio/MusicLoader.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicLoader : MonoBehaviour {
+    [SerializeField]
+    private AudioClip song;
+    public bool PlayOnStart;
+
+    void Start()
+    {
+        if (PlayOnStart)
+        {
+            PlayMusic();
+        }
+    }
+
+    public void PlayMusic()
+    {
+        if (song)
+                AudioManager.Instance.PlayMusic(song);
+            else
+                AudioManager.Instance.StopMusic();
+    }
+}
