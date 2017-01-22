ff0ab309945b889804cdff61f90c4afcb2d187e9
riktothepast <rik@502studios.net>
2017-01-22 14:32:02 -0600

    saves scene changes

--- Assets/Scripts/GameLogic/GameManager.cs
+++ Assets/Scripts/GameLogic/GameManager.cs
@@ -6,19 +6,23 @@ public class GameManager : MonoBehaviour {
     public GameObject winCanvas;
     public DeviceManager deviceManager;
     public float timeToStart = 1f;
+    bool hasGameStarted = false;
 
 	void Awake () {
         Invoke("StartTheGame", timeToStart);
 	}
 	
 	void Update () {
+        if (!hasGameStarted)
+            return;
+
         if (GameObject.FindGameObjectsWithTag("Player").Length <= 0)
         {
             Debug.Log("game over");
             StopTheGame();
             Invoke("GameOver", 5);
         }
-        if (GameObject.FindGameObjectsWithTag("VampiresHearth").Length >= 0)
+        if (GameObject.FindGameObjectsWithTag("VampiresHearth").Length == 0)
         {
             winCanvas.SetActive(true);
             Invoke("GameOver", 5);
@@ -30,6 +34,7 @@ public class GameManager : MonoBehaviour {
     {
         deviceManager.StartPlayers();
         Camera.main.GetComponent<CameraMovement>().moveCamera = true;
+        hasGameStarted = true;
     }
 
     void StopTheGame()
